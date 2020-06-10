10a79313815b60653f41430b4219404b8826b823
Linn Dahlgren <linn.dahlgren@pija.se>
2020-06-10 13:37:28 +0200

    Updated patched adapter to not send partial addresses

--- includes/class-wc-patched-adapter.php
+++ includes/class-wc-patched-adapter.php
@@ -1,6 +1,7 @@
 <?php
 
 use SwedbankPay\Core\Adapter\WC_Adapter;
+use SwedbankPay\Core\OrderInterface;
 use SwedbankPay\Core\Log\LogLevel;
 
 defined('ABSPATH') || exit;
@@ -16,26 +17,62 @@ class WC_Patched_Adapter extends WC_Adapter
    * @param mixed|null $transaction_id
    */
   public function updateOrderStatus($order_id, $status, $message = null, $transaction_id = null) {
-    $order = wc_get_order($order_id);
-    $order->read_meta_data(true);
-    $old_transaction_number = $order->get_meta('_transaction_number');
-    if ($old_transaction_number && $transaction_id) {
-        $this->log(LogLevel::INFO, sprintf("Old transaction id: %s\nNew transaction id: %s", $old_transaction_number, $transaction_id));
-        if ((int) $old_transaction_number >= (int) $transaction_id) {
-            $this->log(LogLevel::WARNING, 'ATTEMPTED TO PROCESS A PREVIOUS TRANSACTION WHEN A LATER ONE HAD ALREADY BEEN PROCESSED!');
-            return;
-        }
-        $this->log(LogLevel::INFO, sprintf("Storing transaction metadata: %s", $transaction_id));
-        $order->update_meta_data('_transaction_number', (string) $transaction_id);
-        $order->save_meta_data();
-    } else if ($transaction_id) {
-        $this->log(LogLevel::INFO, sprintf("Old transaction id: %s\nNew transaction id: %s", $old_transaction_number, $transaction_id));
-        $this->log(LogLevel::INFO, sprintf("Storing transaction metadata: %s", $transaction_id));
-        $order->update_meta_data('_transaction_number', (string) $transaction_id);
-        $order->save_meta_data();
-    } else {
-        $this->log(LogLevel::INFO, sprintf("Old transaction id: %s\nNew transaction id: %s", $old_transaction_number, $transaction_id));
-    }
+    if ( !$order ) return;
     parent::updateOrderStatus($order_id, $status, $message, $transaction_id);
+  }
+
+  /**
+   * Get Order Data.
+   *
+   * @param mixed $order_id
+   *
+   * @return array
+   */
+  public function getOrderData($order_id)
+  {
+      $data = parent::getOrderData($order_id);
+      $validBilling = true;
+      $validBilling = $validBilling && (bool) $data[OrderInterface::BILLING_COUNTRY_CODE];
+      $validBilling = $validBilling && (bool) $data[OrderInterface::BILLING_CITY];
+      $validBilling = $validBilling && (bool) $data[OrderInterface::BILLING_POSTCODE];
+      $validBilling = $validBilling && (bool) $data[OrderInterface::BILLING_FIRST_NAME];
+
+      if (!$validBilling) {
+        $data[OrderInterface::BILLING_COUNTRY] = null;
+        $data[OrderInterface::BILLING_COUNTRY_CODE] = null;
+        $data[OrderInterface::BILLING_ADDRESS1] = null;
+        $data[OrderInterface::BILLING_ADDRESS2] = null;
+        $data[OrderInterface::BILLING_ADDRESS3] = null;
+        $data[OrderInterface::BILLING_CITY] = null;
+        $data[OrderInterface::BILLING_STATE] = null;
+        $data[OrderInterface::BILLING_POSTCODE] = null;
+        $data[OrderInterface::BILLING_PHONE] = null;
+        $data[OrderInterface::BILLING_EMAIL] = null;
+        $data[OrderInterface::BILLING_FIRST_NAME] = null;
+        $data[OrderInterface::BILLING_LAST_NAME] = null;
+      }
+
+      $validShipping = true;
+      $validShipping = $validShipping && (bool) $data[OrderInterface::SHIPPING_COUNTRY_CODE];
+      $validShipping = $validShipping && (bool) $data[OrderInterface::SHIPPING_CITY];
+      $validShipping = $validShipping && (bool) $data[OrderInterface::SHIPPING_POSTCODE];
+      $validShipping = $validShipping && (bool) $data[OrderInterface::SHIPPING_FIRST_NAME];
+
+      if (!$validShipping) {
+        $data[OrderInterface::SHIPPING_COUNTRY] = null;
+        $data[OrderInterface::SHIPPING_COUNTRY_CODE] = null;
+        $data[OrderInterface::SHIPPING_ADDRESS1] = null;
+        $data[OrderInterface::SHIPPING_ADDRESS2] = null;
+        $data[OrderInterface::SHIPPING_ADDRESS3] = null;
+        $data[OrderInterface::SHIPPING_CITY] = null;
+        $data[OrderInterface::SHIPPING_STATE] = null;
+        $data[OrderInterface::SHIPPING_POSTCODE] = null;
+        $data[OrderInterface::SHIPPING_PHONE] = null;
+        $data[OrderInterface::SHIPPING_EMAIL] = null;
+        $data[OrderInterface::SHIPPING_FIRST_NAME] = null;
+        $data[OrderInterface::SHIPPING_LAST_NAME] = null;
+      }
+      
+      return $data;
   }
 }
