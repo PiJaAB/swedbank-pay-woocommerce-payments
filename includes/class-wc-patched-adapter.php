768c11f47f4a4e5ef473f035db6d9052219ae33a
Linn Dahlgren <linn.dahlgren@pija.se>
2020-06-10 13:41:19 +0200

    Revert removal of important double mail issue

--- includes/class-wc-patched-adapter.php
+++ includes/class-wc-patched-adapter.php
@@ -18,6 +18,25 @@ class WC_Patched_Adapter extends WC_Adapter
    */
   public function updateOrderStatus($order_id, $status, $message = null, $transaction_id = null) {
     if ( !$order ) return;
+    $order->read_meta_data(true);
+    $old_transaction_number = $order->get_meta('_transaction_number');
+    if ($old_transaction_number && $transaction_id) {
+        $this->log(LogLevel::INFO, sprintf("Old transaction id: %s\nNew transaction id: %s", $old_transaction_number, $transaction_id));
+        if ((int) $old_transaction_number >= (int) $transaction_id) {
+            $this->log(LogLevel::WARNING, 'ATTEMPTED TO PROCESS A PREVIOUS TRANSACTION WHEN A LATER ONE HAD ALREADY BEEN PROCESSED!');
+            return;
+        }
+        $this->log(LogLevel::INFO, sprintf("Storing transaction metadata: %s", $transaction_id));
+        $order->update_meta_data('_transaction_number', (string) $transaction_id);
+        $order->save_meta_data();
+    } else if ($transaction_id) {
+        $this->log(LogLevel::INFO, sprintf("Old transaction id: %s\nNew transaction id: %s", $old_transaction_number, $transaction_id));
+        $this->log(LogLevel::INFO, sprintf("Storing transaction metadata: %s", $transaction_id));
+        $order->update_meta_data('_transaction_number', (string) $transaction_id);
+        $order->save_meta_data();
+    } else {
+        $this->log(LogLevel::INFO, sprintf("Old transaction id: %s\nNew transaction id: %s", $old_transaction_number, $transaction_id));
+    }
     parent::updateOrderStatus($order_id, $status, $message, $transaction_id);
   }
 
